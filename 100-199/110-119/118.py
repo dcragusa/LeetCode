47544dd16209f532d9a62985bf0f2ed1cee8071a
dcragusa <dcragusa@yahoo.com>
2021-09-20 19:16:43 -0700

    Formatting

--- 100-199/110-119/118.py
+++ 100-199/110-119/118.py
@@ -2,15 +2,12 @@
 Given an integer numRows, return the first numRows rows of Pascal's triangle.
 
 Example 1:
-Input: numRows = 5
-Output: [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
+Input: numRows = 5,  Output: [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
 
 Example 2:
-Input: numRows = 1
-Output: [[1]]
+Input: numRows = 1,  Output: [[1]]
 
-Constraints:
-1 <= numrows <= 30
+Constraints:  1 <= numrows <= 30
 """
 
 """
